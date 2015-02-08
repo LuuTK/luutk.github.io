ed14a5ef7a165882118515f8000df4f2fdd51e75
Tuan-Kiet Luu <tuan-kiet.luu@mail.mcgill.ca>
2015-02-07 20:38:43 -0500

    d

--- js/login.js
+++ js/login.js
@@ -22,6 +22,14 @@ function signinCallback(resp) {
 
 gapi.client.load('plus', 'v1', function() {
   gapi.client.plus.people.get( {'userId' : 'me'} ).execute(function(resp) {
+  	
+  	  if (resp['status']['signed_in']) {
+    var access_token = resp['access_token'];
+    console.log(access_token);
+    localStorage.setItem("accessToken", access_token);
+    // go to main.html
+    window.open("main.html", "_self");
+  	
     // Shows other profile information
     console.log(resp);
   });
