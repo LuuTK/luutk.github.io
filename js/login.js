be284492be1fb81e3b10da18da2ed813f3cbf5bd
Tuan-Kiet Luu <tuan-kiet.luu@mail.mcgill.ca>
2015-02-07 21:24:35 -0500

    logout function

--- js/login.js
+++ js/login.js
@@ -3,18 +3,6 @@
  */
 function signinCallback(resp) {
 
-	var test = false;
-	/*
-	 gapi.client.load('plus', 'v1', apiClientLoaded);
-	 if (resp['status']['signed_in']) {
-	 var access_token = resp['access_token'];
-	 console.log(access_token);
-	 localStorage.setItem("accessToken", access_token);
-	 // go to main.html
-	 window.open("main.html", "_self");
-	 }
-	 */
-
 	gapi.client.load('oauth2', 'v2', function() {
 		gapi.client.oauth2.userinfo.get().execute(function(resp) {
 			var access_token = resp['access_token'];
@@ -27,50 +15,16 @@ function signinCallback(resp) {
 			console.log(JSON.stringify(resp));
 
 			if (resp.id != null && resp.id != 'undefined' && resp.id != "") {
-				alert("You are already logged in");
+				alert("Welcome!");
 				window.open("main.html", "_self");
 
 			} else {
-				alert("id does not exist");
+				alert("Please Log in");
 			}
-
+ 
 		});
 	});
 
-	/*
-	 gapi.client.load('plus', 'v1', function() {
-	 gapi.client.plus.people.get( {'userId' : 'me'} ).execute(function(resp) {
-
-	 // 	  if (resp['status']['signed_in']) {
-	 var access_token = resp['access_token'];
-
-	 localStorage.setItem("accessToken", access_token);
-	 localStorage.setItem("email", resp.email);
-	 localStorage.setItem("id", resp.id);
-	 //localStorage.setItem("id".resp.id);
-
-	 // go to main.html
-	 //   window.open("main.html", "_self");
-	 // 	}
-	 // Shows other profile information
-	 console.log("access token = " + access_token);
-	 console.log(resp);
-	 console.log("email = " + resp.email);
-	 console.log("id = " + resp.id);
-	 console.log(JSON.stringify(localStorage));
-
-	 if(resp.id != null && resp.id != 'undefined' && resp.id != "" && test == true){
-	 alert("id exists");
-	 window.open("main.html", "_self");
-
-	 }else{
-	 alert("id does not exist");
-	 }
-
-	 });
-	 });
-
-	 */
 }
 
 function getAccessToken() {
@@ -132,6 +86,11 @@ function loadFullCalendar() {
 function logout() {
 	var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' + getAccessToken();
 	// Perform an asynchronous GET request.
+	
+	localStorage.removeItem("accessToken");
+	localStorage.removeItem("email");
+	localStorage.removeItem("id");
+	
 	$.ajax({
 		type : 'GET',
 		url : revokeUrl,
