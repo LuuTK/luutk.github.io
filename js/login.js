9343518a9f9ae23bfdef62a206db1a58ef9d7b32
TKLuu <tuan-kiet.luu@mail.mcgill.ca>
2015-02-16 23:52:32 -0500

    fixed bug

--- js/login.js
+++ js/login.js
@@ -122,7 +122,7 @@ function logout() {
 
 
  function getWeather() {
- 	var input_location = document.getElementById("myText").value;
+ 	var input_location = document.getElementById("weather_location").value;
     var url1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"
     var url2 = input_location;
     var url3 = "%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
